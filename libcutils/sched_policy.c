open-vela/external_android_system_core

commit 007b56a1d1c250c5909d751de7601ae23243bee2

Set timer slack value according to thread foreground/background state.
Change-Id: Ia998f349129acf4602b767d1ff94839607fdbdbd
Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>

libcutils/sched_policy.c
@@ -26,6 +26,7 @@
 #include <fcntl.h>
 #include <cutils/sched_policy.h>
 #include <cutils/log.h>
+#include <linux/prctl.h>
 
 /* Re-map SP_DEFAULT to the system default policy, and leave other values unchanged.
  * Call this any place a SchedPolicy is used as an input parameter.
@@ -52,6 +53,9 @@ static inline SchedPolicy _policy(SchedPolicy p)
 #define POLICY_DEBUG 0
 
 #define CAN_SET_SP_SYSTEM 0 // non-zero means to implement set_sched_policy(tid, SP_SYSTEM)
+
+// timer slack value in nS enforced when the thread moves to background
+#define TIMER_SLACK_BG 40000000
 
 static pthread_once_t the_once = PTHREAD_ONCE_INIT;
 
@@ -323,6 +327,8 @@ int set_sched_policy(int tid, SchedPolicy policy)
                             SCHED_BATCH : SCHED_NORMAL,
                            &param);
     }
+
+    prctl(PR_SET_TIMERSLACK_PID, policy == SP_BACKGROUND ? TIMER_SLACK_BG : 0, tid);
 
     return 0;
 }

commit 81ed88d170cc8d9db164629cc24cc1499b922481

Fix build problems caused by commit: 007b56a1d1c250c5909d751de7601ae23243bee2 for non-linux hosts.
Change-Id: I00b9c99b9d9d072f19549b3a9970a27e75cd49b7
Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>

libcutils/sched_policy.c
@@ -26,7 +26,6 @@
 #include <fcntl.h>
 #include <cutils/sched_policy.h>
 #include <cutils/log.h>
-#include <linux/prctl.h>
 
 /* Re-map SP_DEFAULT to the system default policy, and leave other values unchanged.
  * Call this any place a SchedPolicy is used as an input parameter.
@@ -39,6 +38,7 @@ static inline SchedPolicy _policy(SchedPolicy p)
 
 #if defined(HAVE_ANDROID_OS) && defined(HAVE_SCHED_H) && defined(HAVE_PTHREADS)
 
+#include <linux/prctl.h>
 #include <sched.h>
 #include <pthread.h>
 