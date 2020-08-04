31cf17cc18b00ca0698cb27e9e9a2c97e6c6c0d3
openvela-robot <openvela-robot@xiaomi.com>
2020-08-04 23:19:46 +0800

    Merge "libutils: check vsnprintf error" into oc-dev am: 6692df9ad0 am: 2763de354d am: abd218fd3b am: bff9f2955c am: df04782baf am: 85c13439b7
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/core/+/12254459
    
    Change-Id: I1cb83a4a22e205dddd10536f61392215d5cb33dc

--- libcutils/fs_config.h
+++ libcutils/fs_config.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006 The Android Open Source Project
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,17 +17,21 @@
 #pragma once
 
 #include <stdint.h>
-#include <sys/types.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+// Binary format for the runtime <partition>/etc/fs_config_(dirs|files) filesystem override files.
+struct fs_path_config_from_file {
+    uint16_t len;
+    uint16_t mode;
+    uint16_t uid;
+    uint16_t gid;
+    uint64_t capabilities;
+    char prefix[];
+} __attribute__((__aligned__(sizeof(uint64_t))));
 
-#if defined(__GLIBC__) || defined(_WIN32)
-/* Declaration of strlcpy() for platforms that don't already have it. */
-size_t strlcpy(char *dst, const char *src, size_t size);
-#endif
-
-#ifdef __cplusplus
-} // extern "C"
-#endif
+struct fs_path_config {
+    unsigned mode;
+    unsigned uid;
+    unsigned gid;
+    uint64_t capabilities;
+    const char* prefix;
+};
