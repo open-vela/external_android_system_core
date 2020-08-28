b277d7a505a3678ddb386aefe45e088bc04bbcee
liujinye <liujinye@xiaomi.com>
2020-08-29 00:36:46 +0800

    FileMap::create: remove duplicate addition. am: d7c1b7a95d am: 32f7b9bdae am: 80fb4ff1b9 am: de58476a29 am: c8e786493d am: 2f48301f46 am: e8c4c02771
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/core/+/12393457
    
    Change-Id: Id9449666e2878e6316d5dcf8d2892e7fef009b33

--- libutils/Threads.cpp
+++ libutils/Threads.cpp
@@ -302,8 +302,8 @@ void androidSetCreateThreadFunc(android_create_thread_fn func)
 }
 
 #if defined(__ANDROID__)
-int androidSetThreadPriority(pid_t tid, int pri)
-{
+namespace {
+int androidSetThreadPriorityInternal(pid_t tid, int pri, bool change_policy) {
     int rc = 0;
     int lasterr = 0;
     int curr_pri = getpriority(PRIO_PROCESS, tid);
@@ -312,17 +312,19 @@ int androidSetThreadPriority(pid_t tid, int pri)
         return rc;
     }
 
-    if (pri >= ANDROID_PRIORITY_BACKGROUND) {
-        rc = SetTaskProfiles(tid, {"SCHED_SP_BACKGROUND"}, true) ? 0 : -1;
-    } else if (curr_pri >= ANDROID_PRIORITY_BACKGROUND) {
-        SchedPolicy policy = SP_FOREGROUND;
-        // Change to the sched policy group of the process.
-        get_sched_policy(getpid(), &policy);
-        rc = SetTaskProfiles(tid, {get_sched_policy_profile_name(policy)}, true) ? 0 : -1;
-    }
+    if (change_policy) {
+        if (pri >= ANDROID_PRIORITY_BACKGROUND) {
+            rc = SetTaskProfiles(tid, {"SCHED_SP_BACKGROUND"}, true) ? 0 : -1;
+        } else if (curr_pri >= ANDROID_PRIORITY_BACKGROUND) {
+            SchedPolicy policy = SP_FOREGROUND;
+            // Change to the sched policy group of the process.
+            get_sched_policy(getpid(), &policy);
+            rc = SetTaskProfiles(tid, {get_sched_policy_profile_name(policy)}, true) ? 0 : -1;
+        }
 
-    if (rc) {
-        lasterr = errno;
+        if (rc) {
+            lasterr = errno;
+        }
     }
 
     if (setpriority(PRIO_PROCESS, tid, pri) < 0) {
@@ -332,6 +334,15 @@ int androidSetThreadPriority(pid_t tid, int pri)
     }
 
     return rc;
+}
+}  // namespace
+
+int androidSetThreadPriority(pid_t tid, int pri) {
+    return androidSetThreadPriorityInternal(tid, pri, true);
+}
+
+int androidSetThreadPriorityAndPolicy(pid_t tid, int pri, bool change_policy) {
+    return androidSetThreadPriorityInternal(tid, pri, change_policy);
 }
 
 int androidGetThreadPriority(pid_t tid) {
