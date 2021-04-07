70919c39caaab851f44952c74ea87bc84a1c43d1
openvela-robot <openvela-robot@xiaomi.com>
2021-04-08 01:09:49 +0800

    Snap for 7266202 from bef1e8b472ff100fc5df88dbd8f81f2b25c28392 to sc-v2-release
    Change-Id: Ifaecdc0c52a79739520f2d707206eb21f95e0320

--- libutils/include/utils/String16.h
+++ libutils/include/utils/String16.h
@@ -85,6 +85,8 @@ public:
 
             bool                contains(const char16_t* chrs) const;
 
+            status_t            makeLower();
+
             status_t            replaceAll(char16_t replaceThis,
                                            char16_t withThis);
 
