657c52419893393eaba0366e8d6b56355fe02850
openvela-robot <openvela-robot@xiaomi.com>
2021-04-08 18:39:22 +0800

    Merge changes I66c97386,Id205c88d am: 05a7dfa104
    Original change: https://android-review.googlesource.com/c/platform/system/core/+/1668805
    
    Change-Id: I5182d03b7a44b9034ea096233d07708b5de24ad2

--- libutils/String16_test.cpp
+++ libutils/String16_test.cpp
@@ -90,13 +90,6 @@ TEST(String16Test, Insert) {
     EXPECT_STR16EQ(u"VerifyInsert me", tmp);
 }
 
-TEST(String16Test, RemoveDefault) {
-    String16 tmp("Verify me");
-    tmp.remove(4);
-    EXPECT_EQ(4U, tmp.size());
-    EXPECT_STR16EQ(u"Veri", tmp);
-}
-
 TEST(String16Test, Remove) {
     String16 tmp("Verify me");
     tmp.remove(2, 6);
@@ -104,11 +97,11 @@ TEST(String16Test, Remove) {
     EXPECT_STR16EQ(u" m", tmp);
 }
 
-TEST(String16Test, RemoveOutOfBounds) {
-    String16 tmp("Verify me");
-    tmp.remove(100, 6);
-    EXPECT_EQ(3U, tmp.size());
-    EXPECT_STR16EQ(u" me", tmp);
+TEST(String16Test, MakeLower) {
+    String16 tmp("Verify Me!");
+    tmp.makeLower();
+    EXPECT_EQ(10U, tmp.size());
+    EXPECT_STR16EQ(u"verify me!", tmp);
 }
 
 TEST(String16Test, ReplaceAll) {
@@ -180,6 +173,14 @@ TEST(String16Test, StaticStringRemove) {
     tmp.remove(2, 6);
     EXPECT_EQ(2U, tmp.size());
     EXPECT_STR16EQ(u" m", tmp);
+    EXPECT_FALSE(tmp.isStaticString());
+}
+
+TEST(String16Test, StaticStringMakeLower) {
+    StaticString16 tmp(u"Verify me!");
+    tmp.makeLower();
+    EXPECT_EQ(10U, tmp.size());
+    EXPECT_STR16EQ(u"verify me!", tmp);
     EXPECT_FALSE(tmp.isStaticString());
 }
 
