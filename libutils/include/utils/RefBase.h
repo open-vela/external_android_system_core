33b03052f56b16a9a4ab0f5432ba20350a109c09
liujinye <liujinye@xiaomi.com>
2021-04-13 01:11:06 +0800

    Snap for 7277590 from 51ffbced4936305c91097aca6b2d098e7d179080 to sc-v2-release
    Change-Id: I071b21bcdea072a1e0cac589005914b027828afc

--- libutils/include/utils/RefBase.h
+++ libutils/include/utils/RefBase.h
@@ -416,16 +416,13 @@ public:
     wp(std::nullptr_t) : wp() {}
 #else
     wp(T* other);  // NOLINT(implicit)
-    template <typename U>
-    wp(U* other);  // NOLINT(implicit)
-    wp& operator=(T* other);
-    template <typename U>
-    wp& operator=(U* other);
 #endif
-
     wp(const wp<T>& other);
     explicit wp(const sp<T>& other);
 
+#if !defined(ANDROID_UTILS_REF_BASE_DISABLE_IMPLICIT_CONSTRUCTION)
+    template<typename U> wp(U* other);  // NOLINT(implicit)
+#endif
     template<typename U> wp(const sp<U>& other);  // NOLINT(implicit)
     template<typename U> wp(const wp<U>& other);  // NOLINT(implicit)
 
@@ -433,9 +430,15 @@ public:
 
     // Assignment
 
+#if !defined(ANDROID_UTILS_REF_BASE_DISABLE_IMPLICIT_CONSTRUCTION)
+    wp& operator = (T* other);
+#endif
     wp& operator = (const wp<T>& other);
     wp& operator = (const sp<T>& other);
 
+#if !defined(ANDROID_UTILS_REF_BASE_DISABLE_IMPLICIT_CONSTRUCTION)
+    template<typename U> wp& operator = (U* other);
+#endif
     template<typename U> wp& operator = (const wp<U>& other);
     template<typename U> wp& operator = (const sp<U>& other);
 
@@ -556,31 +559,6 @@ wp<T>::wp(T* other)
 {
     m_refs = other ? m_refs = other->createWeak(this) : nullptr;
 }
-
-template <typename T>
-template <typename U>
-wp<T>::wp(U* other) : m_ptr(other) {
-    m_refs = other ? other->createWeak(this) : nullptr;
-}
-
-template <typename T>
-wp<T>& wp<T>::operator=(T* other) {
-    weakref_type* newRefs = other ? other->createWeak(this) : nullptr;
-    if (m_ptr) m_refs->decWeak(this);
-    m_ptr = other;
-    m_refs = newRefs;
-    return *this;
-}
-
-template <typename T>
-template <typename U>
-wp<T>& wp<T>::operator=(U* other) {
-    weakref_type* newRefs = other ? other->createWeak(this) : 0;
-    if (m_ptr) m_refs->decWeak(this);
-    m_ptr = other;
-    m_refs = newRefs;
-    return *this;
-}
 #endif
 
 template<typename T>
@@ -596,6 +574,15 @@ wp<T>::wp(const sp<T>& other)
 {
     m_refs = m_ptr ? m_ptr->createWeak(this) : nullptr;
 }
+
+#if !defined(ANDROID_UTILS_REF_BASE_DISABLE_IMPLICIT_CONSTRUCTION)
+template<typename T> template<typename U>
+wp<T>::wp(U* other)
+    : m_ptr(other)
+{
+    m_refs = other ? other->createWeak(this) : nullptr;
+}
+#endif
 
 template<typename T> template<typename U>
 wp<T>::wp(const wp<U>& other)
@@ -622,6 +609,19 @@ wp<T>::~wp()
     if (m_ptr) m_refs->decWeak(this);
 }
 
+#if !defined(ANDROID_UTILS_REF_BASE_DISABLE_IMPLICIT_CONSTRUCTION)
+template<typename T>
+wp<T>& wp<T>::operator = (T* other)
+{
+    weakref_type* newRefs =
+        other ? other->createWeak(this) : nullptr;
+    if (m_ptr) m_refs->decWeak(this);
+    m_ptr = other;
+    m_refs = newRefs;
+    return *this;
+}
+#endif
+
 template<typename T>
 wp<T>& wp<T>::operator = (const wp<T>& other)
 {
@@ -645,6 +645,19 @@ wp<T>& wp<T>::operator = (const sp<T>& other)
     m_refs = newRefs;
     return *this;
 }
+
+#if !defined(ANDROID_UTILS_REF_BASE_DISABLE_IMPLICIT_CONSTRUCTION)
+template<typename T> template<typename U>
+wp<T>& wp<T>::operator = (U* other)
+{
+    weakref_type* newRefs =
+        other ? other->createWeak(this) : 0;
+    if (m_ptr) m_refs->decWeak(this);
+    m_ptr = other;
+    m_refs = newRefs;
+    return *this;
+}
+#endif
 
 template<typename T> template<typename U>
 wp<T>& wp<T>::operator = (const wp<U>& other)
