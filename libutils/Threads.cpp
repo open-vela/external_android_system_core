106135a233249b9999794483ff7041f4373c7fd0
openvela-robot <openvela-robot@xiaomi.com>
2021-10-06 22:55:12 +0800

    Merge Android 12
    Bug: 202323961
    Merged-In: I6c3beb288ae8733ef65723d8ae5bc820a65f17c2
    Change-Id: I1f1c2bf1a8e11d4cb590a0df4695caa3e16d4ba5

--- libutils/Threads.cpp
+++ libutils/Threads.cpp
@@ -86,10 +86,8 @@ struct thread_data_t {
 
         // A new thread will be in its parent's sched group by default,
         // so we just need to handle the background case.
-        // currently set to system_background group which is different
-        // from background group for app.
         if (prio >= ANDROID_PRIORITY_BACKGROUND) {
-            SetTaskProfiles(0, {"SCHED_SP_SYSTEM"}, true);
+            SetTaskProfiles(0, {"SCHED_SP_BACKGROUND"}, true);
         }
 
         if (name) {
@@ -315,7 +313,7 @@ int androidSetThreadPriority(pid_t tid, int pri)
     }
 
     if (pri >= ANDROID_PRIORITY_BACKGROUND) {
-        rc = SetTaskProfiles(tid, {"SCHED_SP_SYSTEM"}, true) ? 0 : -1;
+        rc = SetTaskProfiles(tid, {"SCHED_SP_BACKGROUND"}, true) ? 0 : -1;
     } else if (curr_pri >= ANDROID_PRIORITY_BACKGROUND) {
         SchedPolicy policy = SP_FOREGROUND;
         // Change to the sched policy group of the process.
@@ -695,7 +693,7 @@ status_t Thread::run(const char* name, int32_t priority, size_t stack)
     mThread = thread_id_t(-1);
 
     // hold a strong reference on ourself
-    mHoldSelf = sp<Thread>::fromExisting(this);
+    mHoldSelf = this;
 
     mRunning = true;
 
