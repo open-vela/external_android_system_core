336d4b4fa2f9ea5e89f4ed3b5144db25281623ea
Xiang Xiao <xiaoxiang@xiaomi.com>
2023-04-11 03:21:15 +0800

    Fix gcc error: cannot convert ‘const android::Vector<TYPE>*’ to ‘android::VectorImpl*’
    /home/xiaoxiang/vela/apps/external/android/libutils/libutils/include/utils/Vector.h:259:64: error: cannot convert ‘const android::Vector<TYPE>*’ to ‘android::VectorImpl*’
      259 |     VectorImpl::operator = (static_cast<const VectorImpl&>(rhs));
          |                                                                ^
    In file included from /home/xiaoxiang/vela/apps/external/android/libutils/libutils/include/utils/Vector.h:25,
                     from /home/xiaoxiang/vela/apps/external/android/libutils/libutils/include/utils/Looper.h:22,
                     from libutils/Looper.cpp:16:
    /home/xiaoxiang/vela/apps/external/android/libutils/libutils/include/utils/VectorImpl.h:56:59: note:   initializing argument 'this' of ‘android::VectorImpl& android::VectorImpl::operator=(const android::VectorImpl&)’
       56 |             VectorImpl&     operator = (const VectorImpl& rhs);
          |                                         ~~~~~~~~~~~~~~~~~~^~~
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- libutils/include/utils/Vector.h
+++ libutils/include/utils/Vector.h
@@ -256,13 +256,13 @@ Vector<TYPE>& Vector<TYPE>::operator = (const Vector<TYPE>& rhs) {
 
 template<class TYPE> inline
 const Vector<TYPE>& Vector<TYPE>::operator = (const Vector<TYPE>& rhs) const {
-    VectorImpl::operator = (static_cast<const VectorImpl&>(rhs));
+    VectorImpl::operator = (rhs);
     return *this;
 }
 
 template<class TYPE> inline
 Vector<TYPE>& Vector<TYPE>::operator = (const SortedVector<TYPE>& rhs) {
-    VectorImpl::operator = (static_cast<const VectorImpl&>(rhs));
+    VectorImpl::operator = (rhs);
     return *this;
 }
 
