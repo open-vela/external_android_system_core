dbce99b3051d6fb59c4251ce7665595c13877475
bijunda1 <bijunda1@xiaomi.com>
2023-06-20 12:12:46 +0800

    Revert "Replace <stdatomic.h> with <atomic> when compiling c++ source code."
    This reverts commit 9fa2639a99dc29c119277156d5ccc1706e300df0.
    
    Reason for revert: causes strange compilation bugs on mac (got x86_64 output on arm64 machine)

--- libcutils/include/cutils/trace.h
+++ libcutils/include/cutils/trace.h
@@ -18,12 +18,7 @@
 #define _LIBS_CUTILS_TRACE_H
 
 #include <inttypes.h>
-#ifdef __cplusplus
-#include <atomic>
-using namespace std;
-#else
 #include <stdatomic.h>
-#endif
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
